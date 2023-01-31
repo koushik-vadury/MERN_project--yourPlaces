64f020319d9743a5d9648d5b94cd31694cffa445
koushik vadury <koushik15-7873@diu.edu.bd>
2023-01-31 11:22:11 +0600

    Adding Lazy-loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
 } from "react-router-dom";
-import NewPlace from "./places/pages/NewPlace";
-import Users from "./user/pages/Users";
 import "./index.css";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
 import { AuthContext } from "./shared/context/auth-context";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 let logoutTimer;
 
@@ -74,36 +76,44 @@ const App = () => {
       <Router>
         <MainNavigation />
         <main>
-          {token ? (
-            <Switch>
-              <Route path="/" exact>
-                <Users />
-              </Route>
-              <Route path="/:userId/places" exact>
-                <UserPlaces />
-              </Route>
-              <Route path="/places/new" exact>
-                <NewPlace />
-              </Route>
-              <Route path="/places/:placeId" exact>
-                <UpdatePlace />
-              </Route>
-              <Redirect to="/" />
-            </Switch>
-          ) : (
-            <Switch>
-              <Route path="/" exact>
-                <Users />
-              </Route>
-              <Route path="/:userId/places" exact>
-                <UserPlaces />
-              </Route>
-              <Route path="/auth" exact>
-                <Auth />
-              </Route>
-              <Redirect to="/auth" />
-            </Switch>
-          )}
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {token ? (
+              <Switch>
+                <Route path="/" exact>
+                  <Users />
+                </Route>
+                <Route path="/:userId/places" exact>
+                  <UserPlaces />
+                </Route>
+                <Route path="/places/new" exact>
+                  <NewPlace />
+                </Route>
+                <Route path="/places/:placeId" exact>
+                  <UpdatePlace />
+                </Route>
+                <Redirect to="/" />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route path="/" exact>
+                  <Users />
+                </Route>
+                <Route path="/:userId/places" exact>
+                  <UserPlaces />
+                </Route>
+                <Route path="/auth" exact>
+                  <Auth />
+                </Route>
+                <Redirect to="/auth" />
+              </Switch>
+            )}
+          </Suspense>
         </main>
       </Router>
     </AuthContext.Provider>
